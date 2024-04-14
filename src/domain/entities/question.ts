d54e826d20f0ae0f0586cf1e18dfb8b4612887d2
Rodrigo Brandão <rodrigo.brandao98@gmail.com>
2024-04-14 12:00:48 -0300

    class: Mapeando relacionamentos

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,14 +1,22 @@
 import { randomUUID } from "node:crypto"
 
+interface QuestionProps {
+  title: string
+  content: string 
+  authorId: string
+}
+
 export class Question {
   public id: string
   public title: string
   public content: string
+  public authorId: string
 
-  constructor(title: string, content: string, id?: string) {
-    this.id = id ?? randomUUID()
+  constructor({title, content, authorId}: QuestionProps, id?: string) {
     this.title = title
+    this.authorId = authorId
     this.content = content
+    this.id = id ?? randomUUID()
   }
 
 }
