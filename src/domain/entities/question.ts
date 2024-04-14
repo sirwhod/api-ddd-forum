437d926074d035ec58c8a972ec051b806fa4db19
Rodrigo Brandão <rodrigo.brandao98@gmail.com>
2024-04-14 12:31:30 -0300

    class: Value Object de slug

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,21 +1,25 @@
 import { randomUUID } from "node:crypto"
+import { Slug } from "./value-objects/slug"
 
 interface QuestionProps {
   title: string
-  content: string 
+  slug: Slug
+  content: string
   authorId: string
 }
 
 export class Question {
   public id: string
   public title: string
+  public slug: Slug
   public content: string
   public authorId: string
 
-  constructor({title, content, authorId}: QuestionProps, id?: string) {
-    this.title = title
-    this.authorId = authorId
-    this.content = content
+  constructor(props: QuestionProps, id?: string) {
+    this.title = props.title
+    this.slug = props.slug
+    this.authorId = props.authorId
+    this.content = props.content
     this.id = id ?? randomUUID()
   }
 
