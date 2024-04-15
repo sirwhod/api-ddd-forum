954d5a51b80f8e3de76dab42a9a32cdfedfb9911
Rodrigo Brandão <rodrigo.brandao98@gmail.com>
2024-04-15 20:54:29 -0300

    class: Caso de uso: Criar pergunta

---
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -0,0 +1,19 @@
+import { QuestionsRepository } from '../repositories/question-repository'
+import { Question } from '../../enterprise/entities/question'
+import { CreateQuestionUseCase } from './create-question'
+
+const fakeQuestionsRepository: QuestionsRepository = {
+  create: async (question: Question) => {},
+}
+
+test('Create an question', async () => {
+  const createQuestion = new CreateQuestionUseCase(fakeQuestionsRepository)
+
+  const { question } = await createQuestion.execute({
+    authorId: '1',
+    title: 'Nova pergunta',
+    content: 'Conteúdo da pergunta',
+  })
+
+  expect(question.id).toBeTruthy()
+})
