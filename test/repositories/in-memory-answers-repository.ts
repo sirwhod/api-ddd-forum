d4fe023fb9812d7719cdb2141601e6452f4b5271
Rodrigo Brandão <rodrigo.brandao98@gmail.com>
2024-04-16 20:48:58 -0300

    class: Caso de uso: Listar respostas da pergunta

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -1,4 +1,5 @@
-import { AnswersRepository } from '@/domain/forum/application/repositories/answares-repository'
+import { PaginationParams } from '@/core/repositories/pagination-params'
+import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
 import { Answer } from '@/domain/forum/enterprise/entities/answer'
 
 export class InMemoryAnswersRepository implements AnswersRepository {
@@ -12,6 +13,14 @@ export class InMemoryAnswersRepository implements AnswersRepository {
     }
 
     return answer
+  }
+
+  async findManyByQuestionId(questionId: string, { page }: PaginationParams) {
+    const answers = this.items
+      .filter((item) => item.questionId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20)
+
+    return answers
   }
 
   async create(answer: Answer) {
