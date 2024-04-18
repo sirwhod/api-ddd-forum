6da0d88d07e19b16a060cb318cf4a4557e9a5133
Rodrigo Brandão <rodrigo.brandao98@gmail.com>
2024-04-18 20:24:04 -0300

    class: Testando classes de erro

--- src/core/either.ts
+++ src/core/either.ts
@@ -1,13 +1,16 @@
-/**
- * Error handler
- *
- * @readonly value: The error message
- * */
-export class Left<L> {
+export class Left<L, R> {
   readonly value: L
 
   constructor(value: L) {
     this.value = value
+  }
+
+  isRight(): this is Right<L, R> {
+    return false
+  }
+
+  isLeft(): this is Left<L, R> {
+    return true
   }
 }
 
@@ -15,21 +18,58 @@ export class Left<L> {
  * Success handler
  *
  * @readonly value: The result of the operation
+ *
+ * @method isRight returns true
+ * @method isLeft returns false
  */
-export class Right<R> {
+export class Right<L, R> {
   readonly value: R
 
   constructor(value: R) {
     this.value = value
   }
+
+  isRight(): this is Right<L, R> {
+    return true
+  }
+
+  isLeft(): this is Left<L, R> {
+    return false
+  }
 }
 
-export type Either<L, R> = Left<L> | Right<R>
+/**
+ * Error type
+ *
+ * @param L - is the type of result if the operation failed
+ * @param R - is the type of result if the operation success
+ *
+ * @example
+ *
+ * type ReturnResponse = Either<string,object>
+ * */
+export type Either<L, R> = Left<L, R> | Right<L, R>
 
+/**
+ * Error handler
+ *
+ * @readonly value: The error message
+ *
+ * @method isRight returns false
+ * @method isLeft returns true
+ * */
 export const left = <L, R>(value: L): Either<L, R> => {
   return new Left(value)
 }
 
+/**
+ * Success handler
+ *
+ * @readonly value: The result of the operation
+ *
+ * @method isRight returns true
+ * @method isLeft returns false
+ */
 export const right = <L, R>(value: R): Either<L, R> => {
   return new Right(value)
 }
