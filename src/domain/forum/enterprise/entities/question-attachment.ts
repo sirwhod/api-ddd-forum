382cddbdc3066d6b593853305e076cdebfad3bb8
Rodrigo Brandão <rodrigo.brandao98@gmail.com>
2024-04-29 20:45:59 -0300

    class: Teste de edição da pergunta

--- src/domain/forum/enterprise/entities/question-attachment.ts
+++ src/domain/forum/enterprise/entities/question-attachment.ts
@@ -1,13 +1,13 @@
 import { Entity } from '@/core/entities/entity'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 
-interface QuestionAttachmentProps {
+export interface QuestionAttachmentProps {
   questionId: UniqueEntityID
   attachmentId: UniqueEntityID
 }
 
 export class QuestionAttachment extends Entity<QuestionAttachmentProps> {
-  get topicId() {
+  get questionId() {
     return this.props.questionId
   }
 
@@ -16,8 +16,8 @@ export class QuestionAttachment extends Entity<QuestionAttachmentProps> {
   }
 
   static create(props: QuestionAttachmentProps, id?: UniqueEntityID) {
-    const attachment = new QuestionAttachment(props, id)
+    const questionAttachment = new QuestionAttachment(props, id)
 
-    return attachment
+    return questionAttachment
   }
 }
