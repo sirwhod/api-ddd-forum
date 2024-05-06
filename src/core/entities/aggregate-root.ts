085165e6f98d8aadade0b5fea93aa09093cd85e8
Rodrigo <rodrigo.brandao98@gmail.com>
2024-05-06 10:44:07 -0300

    class: Testando eventos de domínio

--- src/core/entities/aggregate-root.ts
+++ src/core/entities/aggregate-root.ts
@@ -1,5 +1,5 @@
 import { DomainEvent } from '../events/domain-event'
-import { DomainEvents } from '../events/domain-events.spec'
+import { DomainEvents } from '../events/domain-events'
 import { Entity } from './entity'
 
 export abstract class AggregateRoot<Props> extends Entity<Props> {
--- src/core/events/domain-events.spec.ts
+++ src/core/events/domain-events.spec.ts
@@ -1,81 +1,52 @@
-import { AggregateRoot } from '../entities/aggregate-root'
+import { DomainEvent } from '../events/domain-event'
 import { UniqueEntityID } from '../entities/unique-entity-id'
-import { DomainEvent } from './domain-event'
+import { AggregateRoot } from '../entities/aggregate-root'
+import { DomainEvents } from '@/core/events/domain-events'
+import { vi } from 'vitest'
 
-type DomainEventCallback = (event: unknown) => void
+class CustomAggregateCreated implements DomainEvent {
+  public ocurredAt: Date
+  private aggregate: CustomAggregate // eslint-disable-line
 
-export class DomainEvents {
-  private static handlersMap: Record<string, DomainEventCallback[]> = {}
-  private static markedAggregates: AggregateRoot<unknown>[] = []
-
-  public static markAggregateForDispatch(aggregate: AggregateRoot<unknown>) {
-    const aggregateFound = !!this.findMarkedAggregateByID(aggregate.id)
-
-    if (!aggregateFound) {
-      this.markedAggregates.push(aggregate)
-    }
+  constructor(aggregate: CustomAggregate) {
+    this.aggregate = aggregate
+    this.ocurredAt = new Date()
   }
 
-  private static dispatchAggregateEvents(aggregate: AggregateRoot<unknown>) {
-    aggregate.domainEvents.forEach((event: DomainEvent) => this.dispatch(event))
-  }
-
-  private static removeAggregateFromMarkedDispatchList(
-    aggregate: AggregateRoot<unknown>,
-  ) {
-    const index = this.markedAggregates.findIndex((a) => a.equals(aggregate))
-
-    this.markedAggregates.splice(index, 1)
-  }
-
-  private static findMarkedAggregateByID(
-    id: UniqueEntityID,
-  ): AggregateRoot<unknown> | undefined {
-    return this.markedAggregates.find((aggregate) => aggregate.id.equals(id))
-  }
-
-  public static dispatchEventsForAggregate(id: UniqueEntityID) {
-    const aggregate = this.findMarkedAggregateByID(id)
-
-    if (aggregate) {
-      this.dispatchAggregateEvents(aggregate)
-      aggregate.clearEvents()
-      this.removeAggregateFromMarkedDispatchList(aggregate)
-    }
-  }
-
-  public static register(
-    callback: DomainEventCallback,
-    eventClassName: string,
-  ) {
-    const wasEventRegisteredBefore = eventClassName in this.handlersMap
-
-    if (!wasEventRegisteredBefore) {
-      this.handlersMap[eventClassName] = []
-    }
-
-    this.handlersMap[eventClassName].push(callback)
-  }
-
-  public static clearHandlers() {
-    this.handlersMap = {}
-  }
-
-  public static clearMarkedAggregates() {
-    this.markedAggregates = []
-  }
-
-  private static dispatch(event: DomainEvent) {
-    const eventClassName: string = event.constructor.name
-
-    const isEventRegistered = eventClassName in this.handlersMap
-
-    if (isEventRegistered) {
-      const handlers = this.handlersMap[eventClassName]
-
-      for (const handler of handlers) {
-        handler(event)
-      }
-    }
+  public getAggregateId(): UniqueEntityID {
+    return this.aggregate.id
   }
 }
+
+class CustomAggregate extends AggregateRoot<null> {
+  static create() {
+    const aggregate = new CustomAggregate(null)
+
+    aggregate.addDomainEvent(new CustomAggregateCreated(aggregate))
+
+    return aggregate
+  }
+}
+
+describe('domain events', () => {
+  it('should be able to dispatch and listen to events', async () => {
+    const callbackSpy = vi.fn()
+
+    // Subscriber cadastrado (ouvindo o evento de "resposta criada")
+    DomainEvents.register(callbackSpy, CustomAggregateCreated.name)
+
+    // Estou criando uma resposta porém SEM salvar no banco
+    const aggregate = CustomAggregate.create()
+
+    // Estou assegurando que o evento foi criado porém NÃO foi disparado
+    expect(aggregate.domainEvents).toHaveLength(1)
+
+    // Estou salvando a resposta no banco de dados e assim disparando o evento
+    DomainEvents.dispatchEventsForAggregate(aggregate.id)
+
+    // O subscriber ouve o evento e faz o que precisa ser feito com o dado
+    expect(callbackSpy).toHaveBeenCalled()
+
+    expect(aggregate.domainEvents).toHaveLength(0)
+  })
+})
