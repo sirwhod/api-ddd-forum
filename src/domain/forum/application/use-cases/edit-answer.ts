baf70c39b1e07ed5c8500363955f721d8044d7d0
Rodrigo <rodrigo.brandao98@gmail.com>
2024-05-06 09:31:30 -0300

    class: Caso de uso: ler notificação

--- src/domain/forum/application/use-cases/edit-answer.ts
+++ src/domain/forum/application/use-cases/edit-answer.ts
@@ -1,7 +1,7 @@
 import { Answer } from '@/domain/forum/enterprise/entities/answer'
 import { AnswersRepository } from '../repositories/answers-repository'
-import { NotAllowedError } from './errors/not-allowed-error'
-import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { NotAllowedError } from '../../../../core/errors/errors/not-allowed-error'
+import { ResourceNotFoundError } from '../../../../core/errors/errors/resource-not-found-error'
 import { Either, left, right } from '@/core/either'
 
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
